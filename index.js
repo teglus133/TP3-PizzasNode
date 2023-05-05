717cf40fe2f106ffd022cf3bcd32bc8a6dab5625
XaXhade <you@example.com>
2023-05-04 21:17:03 -0300

    GetByID terminado

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ import { PizzaService } from "./services/pizzaservice.js";
 
 const app = express();
 const port = 3000;
-app.use(express.json())
-app.use(cors())
-
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const elLogger = (req, res, next) => {
     console.log("hello middlewere", Date().toString())
@@ -17,8 +17,18 @@ app.get('/pizza',async (req, res) => {
     const pizza = await PizzaService.getAll()
     res.status(200).send(pizza);
 })
+
+
+app.get('/pizza/:id', async (req, res) => {
+    const pizza = await PizzaService.getById(req.params.id)
+    res.status(200).send(pizza)
+})
+
+
+
 app.post('/pizza',async (req, res) => {
-    console.log("en post, req:", req)
+    console.log("hola")
+    res.json(req.body)
     try {
         await PizzaService.insert(req.body)
         res.status(200).json({ message: 'Pizza creada' })
@@ -27,6 +37,7 @@ app.post('/pizza',async (req, res) => {
         res.status(500).json({error: 'Fallo el insert' })
     }
 })
+
 app.get('/error',(req,res) => {
     res.status(404).send('No se encuentra, sorry!')
 })
--- services/pizzaservice.js
+++ services/pizzaservice.js
@@ -8,11 +8,27 @@ export class PizzaService {
             let pool = await sql.connect(config);
             let result = await pool.request()
                                 .query('SELECT * FROM Pizzas')
-            return result.recordsets[0][0];
+            return result.recordsets;
         }
         catch (error) {
             console.log(error); 
-        }
-        
+        }  
+    
+    }
+    static getById = async(id) => {
+        try {
+            let pool = await sql.connect(config);
+            let result = await pool.request()
+                                .input('pid', sql.Int, id)
+                                .query('SELECT * FROM Pizzas WHERE id = @pid')
+                                
+            return result.recordsets[0][0];
+        } 
+        catch (error) {
+            console.log(error);
+        }   
+    }
+    static insert = async(pizza) => {
+
     }
 }
